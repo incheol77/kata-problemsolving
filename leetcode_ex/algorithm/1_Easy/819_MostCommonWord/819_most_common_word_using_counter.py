dc0ccf1802c03747892a03e3fd208f2d282e24dd
incheol77 <incheol77@gmail.com>
2020-09-15 23:44:18 +0900

    add solutions for leetcode 819 most common word

---
+++ leetcode_ex/algorithm/1_Easy/819_MostCommonWord/819_most_common_word_using_counter.py
@@ -0,0 +1,28 @@
+import collections
+from typing import List
+import re
+
+
+class Solution:
+    def most_common_word(self, paragraph: str, banned: List[str]) -> str:
+        words = [word for word in re.sub('[^\w]', ' ', paragraph)
+            .lower().split() if word not in banned]
+        counts = collections.Counter(words)
+        print(counts)
+        return max(counts, key=counts.get)
+
+
+def main():
+    sol = Solution()
+    paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+    banned = ["hit"]
+    output = "ball"
+    if output == sol.most_common_word(paragraph, banned):
+        print(True)
+    else:
+        print(False)
+
+
+if __name__ == "__main__":
+    main()
+
