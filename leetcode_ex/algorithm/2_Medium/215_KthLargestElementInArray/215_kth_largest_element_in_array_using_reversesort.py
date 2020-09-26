5668596cd943135a705ecc8767fb11ff810d5a22
incheol77 <incheol77@gmail.com>
2020-09-26 17:53:26 +0900

    add solutions of leetcode 215 kth largest element in array

---
+++ leetcode_ex/algorithm/2_Medium/215_KthLargestElementInArray/215_kth_largest_element_in_array_using_reversesort.py
@@ -0,0 +1,33 @@
+import collections
+import heapq
+from typing import List
+import time
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        nums.sort(reverse=True)
+        maxK = 0
+        for i in range(0, k):
+            maxK = nums[i]
+        return maxK
+
+
+def main():
+    sol = Solution()
+    inputs = [[3,2,1,5,6,4], [3,2,3,1,2,4,5,5,6]]
+    ks = [2, 4]
+    outputs = [5, 4]
+
+    for i in range(len(inputs)):
+        start = time.time()
+        res = sol.findKthLargest(inputs[i], ks[i])
+        end = time.time()
+        if outputs[i] == res:
+            print("true : ", end-start, " sec")
+        else:
+            print("false : ", end-start, " sec")
+
+
+if __name__ == "__main__":
+    main()
+
